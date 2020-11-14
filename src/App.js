5f1e35d534d267dbf9ac3a5f576c3ef8817d0d7f
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-11-14 20:16:44 +0530

    feat: grommet added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Grommet, Button, Heading, Box } from 'grommet';
+import { Notification } from 'grommet-icons';
 
+const theme = {
+  global: {
+    colors: {
+      brand: '#8ac6d1',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => {
+  return (
+    <Box
+      tag='header'
+      direction='row'
+      align='center'
+      justify='between'
+      background='brand'
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation='medium'
+      style={{ zIndex: '1' }}
+      {...props}
+    />
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>
+            Collector
+          </Heading>
+          <Button icon={<Notification />} onClick={() => {}} />
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app body
+          </Box>
+          <Box
+            width='medium'
+            background='light-2'
+            elevation='small'
+            align='center'
+            justify='center'
+          >
+            sidebar
+          </Box>
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
 
