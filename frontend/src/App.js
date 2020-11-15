0b1ae118364ebdb010083fef3aa436f5815cae1d
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-11-16 01:24:14 +0530

    feat: ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,12 +58,9 @@ const style = {
 
 const modalStyle = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    margin: 'auto',
+    width: '60%',
+    background: '#555555'
   },
 };
 
