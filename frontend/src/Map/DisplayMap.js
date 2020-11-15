5db8e52773409b118ad617509a760896c8a179b1
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-11-15 14:52:51 +0530

    feat: show info on marker click

--- frontend/src/Map/DisplayMap.js
+++ frontend/src/Map/DisplayMap.js
@@ -3,7 +3,7 @@ import { useLayoutEffect, useRef } from 'react';
 
 import addMarkers from './addMarkers';
 
-const DisplayMap = ({ markers }) => {
+const DisplayMap = ({ markers, setCurrMarker }) => {
   // Create a reference to the HTML element we want to put the map on
   const mapRef = useRef(null);
 
@@ -28,19 +28,20 @@ const DisplayMap = ({ markers }) => {
       pixelRatio: window.devicePixelRatio || 1,
     });
 
-    // Add markers
-    addMarkers(hMap, H, markers);
-
+    
     const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(hMap));
-
+    
     const ui = H.ui.UI.createDefault(hMap, defaultLayers);
+    
+    // Add markers
+    addMarkers(hMap, H, ui, markers, setCurrMarker);
 
     // This will act as a cleanup to run once this hook runs again.
     // This includes when the component un-mounts
     return () => {
       hMap.dispose();
     };
-  }, [mapRef, markers]); // This will run this hook every time this ref is updated
+  }, [mapRef, markers, setCurrMarker]); // This will run this hook every time this ref is updated
 
   return (
     <div
