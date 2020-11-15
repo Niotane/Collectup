29e1750436bf009560953504680f22b8a90a04e8
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-11-15 15:15:41 +0530

    feat: shortest routing through points added

--- frontend/src/Map/DisplayMap.js
+++ frontend/src/Map/DisplayMap.js
@@ -28,13 +28,16 @@ const DisplayMap = ({ markers, setCurrMarker }) => {
       pixelRatio: window.devicePixelRatio || 1,
     });
 
-    
     const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(hMap));
-    
+
     const ui = H.ui.UI.createDefault(hMap, defaultLayers);
-    
+
     // Add markers
     addMarkers(hMap, H, ui, markers, setCurrMarker);
+
+    let router = platform.getRoutingService(null, 8);
+
+    createRoute(H, hMap, markers, router);
 
     // This will act as a cleanup to run once this hook runs again.
     // This includes when the component un-mounts
@@ -53,3 +56,40 @@ const DisplayMap = ({ markers, setCurrMarker }) => {
 };
 
 export default DisplayMap;
+
+function createRoute(H, hMap, markers, router) {
+  const locations = markers.map(
+    ({ location }) => `${location.lat},${location.lng}`
+  );
+
+  router.calculateRoute(
+    {
+      origin: '48.86,2.31',
+      destination: '48.86,2.31',
+      // defines multiple waypoints
+      via: new H.service.Url.MultiValueQueryParameter(locations),
+      // returns route shape as a polyline in response
+      return: 'polyline',
+      transportMode: 'car',
+    },
+    (result) => {
+      const sections = result.routes[0].sections;
+      const lineStrings = [];
+      sections.forEach((section) => {
+        // convert Flexible Polyline encoded string to geometry
+        lineStrings.push(
+          H.geo.LineString.fromFlexiblePolyline(section.polyline)
+        );
+      });
+      const multiLineString = new H.geo.MultiLineString(lineStrings);
+      const bounds = multiLineString.getBoundingBox();
+      // render route on the map
+      hMap.addObject(
+        new H.map.Polyline(multiLineString, { style: { lineWidth: 5 } })
+      );
+      // zoom to polyline
+      hMap.getViewModel().setLookAtData({ bounds });
+    },
+    console.error
+  );
+}
