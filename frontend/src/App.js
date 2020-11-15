7f05f34b51df98112f659aae4e531be2ebf0d770
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-11-15 18:54:35 +0530

    feat ui: progress and feed skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,18 @@ import {
   Form,
   FormField,
   TextInput,
+  Text,
+  Clock,
+  Meter,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  Footer,
+  Anchor,
+  Header,
 } from 'grommet';
-import { Notification } from 'grommet-icons';
+import { Notification, Favorite, ShareOption } from 'grommet-icons';
 import ScaleLoader from 'react-spinners/ScaleLoader';
 
 import DisplayMap from './Map/DisplayMap';
@@ -64,13 +74,13 @@ function App() {
 
   return (
     <Grommet theme={theme} themeMode='dark' full>
-      <Box fill>
-        <AppBar>
-          <Heading level='2' margin='none'>
-            CollectUp.io
-          </Heading>
-          <Button icon={<Notification />} onClick={() => {}} />
-        </AppBar>
+      <AppBar>
+        <Heading level='2' margin='none'>
+          CollectUp.io
+        </Heading>
+        <Button icon={<Notification />} onClick={() => {}} />
+      </AppBar>
+      <Box height='60%'>
         <Suspense fallback={<ScaleLoader loading={isLoading} />}>
           <Box flex direction='row' elevation='small' height={{ min: '30vw' }}>
             <DisplayMap
@@ -81,35 +91,115 @@ function App() {
             />
           </Box>
         </Suspense>
-        <Box
-          flex
-          direction='row'
-          background='light-2'
-          align='center'
-          justify='center'
+      </Box>
+      <Box
+        flex
+        direction='row'
+        background='dark-2'
+        align='baseline'
+        justify='evenly'
+        height='300px'
+        pad='2em'
+      >
+        <Form
+          value={query}
+          onReset={() => setQuery({})}
+          onSubmit={(evt) => {
+            const data = new FormData(evt.target);
+            setQuery(data.get('address'));
+          }}
         >
-          <Form
-            value={query}
-            onReset={() => setQuery({})}
-            onSubmit={(evt) => {
-              const data = new FormData(evt.target);
-              setQuery(data.get('address'));
-            }}
+          <FormField
+            name='current-location'
+            htmlfor='text-input-id'
+            label='Enter your start and end address'
           >
-            <FormField
-              name='current-location'
-              htmlfor='text-input-id'
-              label='Enter address'
-            >
-              <TextInput id='text-input' name='address' />
-            </FormField>
-            <Box direction='row' gap='medium'>
-              <Button type='submit' primary label='Submit' />
-              <Button type='reset' label='Reset' />
-            </Box>
-          </Form>
+            <TextInput id='text-input' name='address' />
+          </FormField>
+          <Box direction='row' gap='medium'>
+            <Button type='submit' primary label='Submit' />
+            <Button type='reset' label='Reset' />
+          </Box>
+        </Form>
+        <Box flex direction='row' justify='evenly' background='dark-2'>
+          <Box gap='0.5vw' alignSelf='start'>
+            <Text weight='bold'> Current time: </Text>
+            <br />
+            <Clock type='digital' size='xxlarge' />
+          </Box>
+          <Box gap='0.5vw' justify='end'>
+            <Text weight='bold'> Journey progress: </Text>
+            <br />
+            <Meter
+              type='circle'
+              values={[
+                {
+                  value: 60,
+                  label: 'sixty',
+                  onClick: () => {},
+                },
+              ]}
+              aria-label='meter'
+              size='xsmall'
+            />
+          </Box>
         </Box>
       </Box>
+      <Box
+        flex
+        direction='row'
+        background='dark-1'
+        align='baseline'
+        justify='evenly'
+        height='1000px'
+        pad='2em'
+      >
+        <Header> User Feeds </Header>
+        <Card height='small' width='small' background='light-1'>
+          <CardHeader pad='medium'>Header</CardHeader>
+          <CardBody pad='medium'>Body</CardBody>
+          <CardFooter pad={{ horizontal: 'small' }} background='light-2'>
+            <Button icon={<Favorite color='red' />} hoverIndicator />
+            <Button icon={<ShareOption color='plain' />} hoverIndicator />
+          </CardFooter>
+        </Card>
+        <Card height='small' width='small' background='light-1'>
+          <CardHeader pad='medium'>Header</CardHeader>
+          <CardBody pad='medium'>Body</CardBody>
+          <CardFooter pad={{ horizontal: 'small' }} background='light-2'>
+            <Button icon={<Favorite color='red' />} hoverIndicator />
+            <Button icon={<ShareOption color='plain' />} hoverIndicator />
+          </CardFooter>
+        </Card>
+        <Card height='small' width='small' background='light-1'>
+          <CardHeader pad='medium'>Header</CardHeader>
+          <CardBody pad='medium'>Body</CardBody>
+          <CardFooter pad={{ horizontal: 'small' }} background='light-2'>
+            <Button icon={<Favorite color='red' />} hoverIndicator />
+            <Button icon={<ShareOption color='plain' />} hoverIndicator />
+          </CardFooter>
+        </Card>
+        <Card height='small' width='small' background='light-1'>
+          <CardHeader pad='medium'>Header</CardHeader>
+          <CardBody pad='medium'>Body</CardBody>
+          <CardFooter pad={{ horizontal: 'small' }} background='light-2'>
+            <Button icon={<Favorite color='red' />} hoverIndicator />
+            <Button icon={<ShareOption color='plain' />} hoverIndicator />
+          </CardFooter>
+        </Card>
+        <Card height='small' width='small' background='light-1'>
+          <CardHeader pad='medium'>Header</CardHeader>
+          <CardBody pad='medium'>Body</CardBody>
+          <CardFooter pad={{ horizontal: 'small' }} background='light-2'>
+            <Button icon={<Favorite color='red' />} hoverIndicator />
+            <Button icon={<ShareOption color='plain' />} hoverIndicator />
+          </CardFooter>
+        </Card>
+      </Box>
+      <Footer background='brand' pad='medium'>
+        <Text>Copyright</Text>
+        <Anchor label='About' />
+      </Footer>
     </Grommet>
   );
 }
