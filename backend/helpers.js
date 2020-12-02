346e85e44bc6a8c9bf9bb8770080f631b22281f5
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-12-02 20:10:32 +0530

    feat: add gcs upload helper

---
+++ backend/helpers.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { Storage } = require('@google-cloud/storage');
+const { v4 } = require('uuid');
+
+const storage = new Storage();
+
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET); // should be your bucket name
+
+/**
+ *
+ * @param { File } object file object that will be uploaded
+ * @description - This function does the following
+ * - It uploads a file to the image bucket on Google Cloud
+ * - It accepts an object as an argument with the
+ *   "originalname" and "buffer" as keys
+ */
+
+const uploadImage = (file) => {
+  return new Promise((resolve, reject) => {
+    const { originalname, buffer } = file;
+
+    const blob = bucket.file(`${originalname.replace(/ /g, '_')}_${v4()}`);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+    });
+    blobStream
+      .on('finish', () => {
+        const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+        resolve(publicUrl);
+      })
+      .on('error', (err) => {
+        console.log(err);
+        reject(`Unable to upload image, something went wrong`);
+      })
+      .end(buffer);
+  });
+};
+
+module.exports = uploadImage;
