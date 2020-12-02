04ee1f707b72b945bf1f5102c7727385f1b7e561
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-12-02 22:11:25 +0530

    feat: header view space evenly

--- frontend/src/components/Header/HeaderView.js
+++ frontend/src/components/Header/HeaderView.js
@@ -9,21 +9,14 @@ import {
 import { Notifications, Menu } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: '#9400D3	',
-  },
-  titleHeading: {
-    padding: '0px 10px',
-    fontSize: '1.5rem',
-  },
-  grow: {
+  title: {
+    fontFamily: `'Montserrat', sans-serif`,
+    fontWeight: '800',
+    fontSize: '1.8em',
     flexGrow: 1,
   },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
+  menuButton: {
+    marginRight: theme.spacing(2),
   },
 }));
 
@@ -31,32 +24,26 @@ function HeaderView() {
   const classes = useStyles();
 
   return (
-    <div className={classes.grow}>
-      <AppBar position='static' className={classes.root}>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-          >
-            <Menu />
-          </IconButton>
-          <Typography variant='h6' color='inherit'>
-            CollectUp
-          </Typography>
-          <div className={classes.grow}>
-            <div className={classes.sectionDesktop}>
-              <IconButton>
-                <Badge badgeContent={4} color='secondary'>
-                  <Notifications />
-                </Badge>
-              </IconButton>
-            </div>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position='static'>
+      <Toolbar>
+        <IconButton
+          edge='start'
+          className={classes.menuButton}
+          color='inherit'
+          aria-label='menu'
+        >
+          <Menu />
+        </IconButton>
+        <Typography variant='h6' color='inherit' className={classes.title}>
+          CollectUp
+        </Typography>
+        <IconButton>
+          <Badge badgeContent={4} color='secondary'>
+            <Notifications />
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
 
