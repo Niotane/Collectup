35bd7774192ca8fc8f63fda3f3b8cdc72eef9a80
Niotane <niotane.beine@gmail.com>
2020-12-07 16:01:03 +0100

    Grid fixed

--- frontend/src/components/Map/FeedView.js
+++ frontend/src/components/Map/FeedView.js
@@ -21,17 +21,13 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '40vh',
-    minHeight: '40vh',
     backgroundColor: '#268386',
-    margin: 'sm',
-    flexJustify: 'around',
-    direction: 'row',
+    minHeight: '70vh',
   },
   media: {
     component: 'img',
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '75%',
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -48,6 +44,11 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '30vw',
     padding: theme.spacing(2),
   },
+  gridContainer: {
+    paddingLeft: '10%',
+    paddingRight: '10%',
+    paddingTop: '2%',
+  },
   fixedCard: {
     height: '3vh',
     overflowY: 'hide',
@@ -56,6 +57,7 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: `'Montserrat', sans-serif`,
     fontWeight: 'bold',
     color: 'white',
+    padding: '20px',
   },
   subheading: {
     fontFamily: `'Montserrat', sans-serif`,
@@ -76,21 +78,18 @@ export default function FeedView({ posts }) {
 
   return (
     <Grid className={classes.box} container>
-      <Grid item container spacing={4} xs={12}>
-        <Grid item xs={5} />
-        <Grid item xs={3}>
-          <Typography variant='h2' className={classes.heading}>
-            MY FEED
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-          <Grid />
-        </Grid>
-        <Grid item>
-          <Grid container spacing={2}>
-            <Posts posts={posts} />
-          </Grid>
+      <Grid item container spacing={4} xs={12} justify='center'>
+        <Typography variant='h2' className={classes.heading}>
+          MY FEED
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Divider />
+        <Grid />
+      </Grid>
+      <Grid item>
+        <Grid container spacing={2} className={classes.gridContainer}>
+          <Posts posts={posts} />
         </Grid>
       </Grid>
     </Grid>
@@ -109,7 +108,7 @@ function Posts({ posts }) {
 
   return posts.map((post) => {
     return (
-      <Grid item key={JSON.stringify(post)} lg={3}>
+      <Grid item key={JSON.stringify(post)} xs={12} sm={4}>
         <Card className={classes.root} key={JSON.stringify(post)}>
           <CardMedia
             alt='Contemplative Reptile'
