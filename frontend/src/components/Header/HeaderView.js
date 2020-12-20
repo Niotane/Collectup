9bcac84dbbbbf9175f78985106592bb177941910
Niotane <niotane.beine@gmail.com>
2020-12-20 15:50:41 +0100

    Reponsive Grid Header

--- frontend/src/components/Header/HeaderView.js
+++ frontend/src/components/Header/HeaderView.js
@@ -6,6 +6,8 @@ import {
   Typography,
   makeStyles,
   Box,
+  Slide,
+  useScrollTrigger,
 } from '@material-ui/core';
 
 import { Notifications, Menu, LocalShipping, Map } from '@material-ui/icons';
@@ -15,7 +17,6 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: `'Montserrat', sans-serif`,
     fontWeight: '800',
     fontSize: '1.8em',
-    flexGrow: 1,
     display: 'none',
     color: theme.palette.grey[200],
     letterSpacing: theme.spacing(0.3),
@@ -24,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(1),
   },
   body1: {
     fontFamily: `'Montserrat', sans-serif`,
@@ -34,86 +35,97 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.grey[200],
   },
   logoImg: {
-    maxWidth: 40,
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    marginRight: theme.spacing(1),
     maxHeight: 50,
-    position: 'absolute',
-    alignItems: 'center',
-    display: 'flex',
-    paddingRight: '12px',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
   },
   grow: {
     flexGrow: 1,
   },
 }));
-
-function HeaderView({ setDriverView, driverView }) {
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+  return (
+    <Slide appear={false} direction='down' in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+function HeaderView({ setDriverView, driverView, props }) {
   const classes = useStyles();
-
   return (
     <div className={classes.grow}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-          >
-            <Menu />
-          </IconButton>
-          <div className={classes.logoImg}>
-            <img
-              src='https://i.ibb.co/5TbtcBc/test.png'
-              alt='logo'
-              className={classes.logoImg}
-            />
-          </div>
-          <Typography
-            variant='h6'
-            color='inherit'
-            noWrap
-            className={classes.title}
-          >
-            COLLECT-UP
-          </Typography>
-
-          <div>
-            <IconButton onClick={() => setDriverView((v) => !v)}>
-              {driverView && (
-                <>
-                  <Typography
-                    variant='body1'
-                    color='inherit'
-                    className={classes.body1}
-                  >
-                    Go to Maps View
-                  </Typography>
-                  <Box m={3} />
-                  <Map style={{ color: 'whitesmoke' }} />
-                </>
-              )}
-              {!driverView && (
-                <>
-                  <Typography
-                    variant='body1'
-                    color='inherit'
-                    className={classes.body1}
-                  >
-                    Driver View
-                  </Typography>
-                  <Box m={1} />
-                  <LocalShipping style={{ color: 'whitesmoke' }} />
-                </>
-              )}
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              edge='start'
+              className={classes.menuButton}
+              color='inherit'
+              aria-label='menu'
+            >
+              <Menu />
             </IconButton>
-            <IconButton>
-              <Badge badgeContent={4} color='secondary'>
-                <Notifications />
-              </Badge>
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
+            <div className={classes.logoImg}>
+              <img
+                src='https://i.ibb.co/5TbtcBc/test.png'
+                alt='logo'
+                className={classes.logoImg}
+              />
+            </div>
+            <Typography
+              variant='h6'
+              color='inherit'
+              noWrap
+              className={classes.title}
+            >
+              COLLECT-UP
+            </Typography>
+            <div className={classes.grow} />
+            <div>
+              <IconButton onClick={() => setDriverView((v) => !v)}>
+                {driverView && (
+                  <>
+                    <Typography
+                      variant='body1'
+                      color='inherit'
+                      className={classes.body1}
+                    >
+                      Go to Maps View
+                    </Typography>
+                    <Box m={3} />
+                    <Map style={{ color: 'whitesmoke' }} />
+                  </>
+                )}
+                {!driverView && (
+                  <>
+                    <Typography
+                      variant='body1'
+                      color='inherit'
+                      className={classes.body1}
+                    >
+                      Driver View
+                    </Typography>
+                    <Box m={1} />
+                    <LocalShipping style={{ color: 'whitesmoke' }} />
+                  </>
+                )}
+              </IconButton>
+              <IconButton>
+                <Badge badgeContent={4} color='secondary'>
+                  <Notifications />
+                </Badge>
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
     </div>
   );
 }
